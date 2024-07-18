ffe9cb65d3934b93d471871a587d8377b9330fa3
Kirill Kanashevich <kirka1812@gmail.com>
2024-07-18 18:12:16 +0300

    Done

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        System.out.print((char)99);
+        System.out.print((char)104);
+        System.out.print((char)97);
+        System.out.print((char)114);
     }
 }
